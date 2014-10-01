f0bf4df1a93955faea17ceb9cdef472cb8257720
Ricardo Tomasi <ricardobeat@gmail.com>
2014-10-01 14:09:35 +0200

    wip

--- channels.js
+++ channels.js
@@ -2,6 +2,15 @@
 
     var uid   = 0
     var slice = Array.prototype.slice
+    var EventEmitter
+
+    var registeredChannels = {}
+
+    if (typeof require !== 'undefined') {
+        EventEmitter = require('./events')
+    } else {
+        EventEmitter = this.Emmy || this.EventEmitter
+    }
 
     function _bind (fn, context, args) {
         return function () {
@@ -9,19 +18,40 @@
         }
     }
 
-    function EventChannel (options) {
-        options || (options = {})
-        this.name    = options.name || (+new Date).toString(32)
-        this.emitter = new EventEmitter({ context: this })
-        this.events  = options.events || {}
-    }
-
     function validateEvent (channel, method, name) {
-        if (!this.events[name]) {
+        if (channel.strict && !channel.events[name]) {
             throw new Error(
                 method + "(): event '" + name + "' has not been registered for channel " + channel.name
             )
         }
+    }
+
+    function EventChannel (name, options) {
+        if (typeof name === 'object') {
+            options = name
+            name = options.name
+        }
+
+        var options = options || {}
+
+        if (!name) {
+            throw new Error('Channel must have a name.')
+        }
+
+        if (registeredChannels[name]) {
+            throw new Error('Channel \'' + name + '\' already exists.')
+        } else {
+            registeredChannels[name] = this
+        }
+
+        if (options.strict !== false && !options.events) {
+            throw new Error('Channel ' + name + ' is in strict mode but no events have been defined')
+        }
+
+        this.name    = name || options.name || (+new Date).toString(32)
+        this.emitter = new EventEmitter({ context: this })
+        this.events  = options.events || {}
+        this.strict  = options.strict === false ? false : true
     }
 
     EventChannel.prototype.subscribe = function (name, fn) {
@@ -39,19 +69,45 @@
         this.emitter.removeListener(name, fn)
     }
 
-    EventChannel.create = function (options) {
-        return new Channel(options)
+    EventChannel.prototype.destroy = function (name) {
+        this.emitter.destroy()
+        delete registeredChannels[name]
     }
+
+    EventChannel.create = function (name, options) {
+        return new EventChannel(name, options)
+    }
+
+    // Channel manager
+    // ------------------------------------------------------------------------
+    
+    function channel (name, options) {
+        if (options === undefined && channel.channels[name]) {
+            return channel.channels[name]
+        } else if (name && typeof options === 'object' && channel.channels[name] === undefined) {
+            var channel = new EventChannel(name, options)
+            channel.channels[name] = channel
+            return channel
+        } else if (channel.channels[name]) {
+            throw new Error('Channel ' + name + ' is already defined.')
+        } else {
+            throw new Error('Channel ' + name + ' does not exist.')
+        }
+    }
+
+    channel.channels = []
 
     // Export commonJS / global / AMD
     // ------------------------------------------------------------------------
     
     if (typeof module !== 'undefined' && module.exports) {
-        module.exports = EventChannel
+        module.exports = { EventChannel: EventChannel, channel: channel }
     } else if (typeof define !== 'undefined') {
-        define('EventChannel', function () { return EventChannel })
+        define('channel/EventChannel', function () { return EventChannel })
+        define('channel', function () { return channel })
     } else {
         this.EventChannel = EventChannel
+        this.channel = channel
     }
 
 }).call(this)
